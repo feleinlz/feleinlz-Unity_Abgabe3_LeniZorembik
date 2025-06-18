65ac91891bc2ab7562b00c07578c91b1e1bd518d
LAPTOP-FJ1RAG0H\leniz <leni.zorembik@gmail.com>
2025-06-18 21:05:05 +0200

    added jump mechanic

--- Assets/+++WORKDATA+++/scripts/playerMovement.cs
+++ Assets/+++WORKDATA+++/scripts/playerMovement.cs
@@ -5,12 +5,56 @@ public class playerMovement : MonoBehaviour
     public Rigidbody2D playerRB;
     public float speed;
     public float input;
+    public SpriteRenderer playerSprite;
+    public float jumpForce;
+    public LayerMask groundLayer;
+    private bool grounded;
+    public Transform feetPosition;
+    public float groundCheckRadius;
 
+    public float jumpTime = 0.35f;
+    public float jumpTimeCounter;
+    private bool isJumping;
 
     // Update is called once per frame
     void Update()
     {
         input = Input.GetAxisRaw("Horizontal");
+        if (input < 0)
+        {
+            playerSprite.flipX = true;
+        }
+        else if (input > 0)
+        {
+            playerSprite.flipX = false;
+        }
+        
+        grounded = Physics2D.OverlapCircle(feetPosition.position, groundCheckRadius, groundLayer);
+
+        if (grounded && Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
+            jumpTimeCounter = jumpTime;
+            playerRB.linearVelocity = Vector2.up * jumpForce;
+        }
+
+        if (Input.GetButton("Jump") && isJumping == true)
+        {
+            if (jumpTimeCounter > 0)
+            {
+                playerRB.linearVelocity = Vector2.up * jumpForce;
+                jumpTimeCounter -= Time.deltaTime;
+            }
+            else
+            {
+                isJumping = false;
+            }
+        }
+        
+        if (Input.GetButtonUp("Jump"))
+        {
+            isJumping = false;
+        }
     }
 
     void FixedUpdate()
